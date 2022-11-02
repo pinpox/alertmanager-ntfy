1beeb0a7b6d8770569dfc42c67b85c6bc779813c
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-11-02 16:50:31 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -10,53 +10,52 @@
     };
   };
 
-  outputs = { nixpkgs, flake-utils, self, ... }:
+  outputs = { nixpkgs, flake-utils, self, ... }: {
+    nixosModules.default = ({ pkgs, ... }: {
+      imports = [ ./module.nix ];
+      # defined overlays injected by the nixflake
+      nixpkgs.overlays = [
+        (_self: _super: {
+          alertmanager-ntfy = self.packages.${pkgs.system}.alertmanager-ntfy;
+        })
+      ];
+    });
+  } //
 
-    {
-      nixosModule = ({ pkgs, ... }: {
-        imports = [ ./module.nix ];
-        # defined overlays injected by the nixflake
-        nixpkgs.overlays = [
-          (_self: _super: {
-            alertmanager-ntfy = self.packages.${pkgs.system}.alertmanager-ntfy;
-          })
-        ];
-      });
-    } //
+  flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      packages = flake-utils.lib.flattenTree rec {
 
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree rec {
+        default = alertmanager-ntfy;
 
-          alertmanager-ntfy = pkgs.buildGoModule rec {
+        alertmanager-ntfy = pkgs.buildGoModule rec {
 
-            pname = "alertmanager-ntfy";
-            version = "1.0.0";
+          pname = "alertmanager-ntfy";
+          version = "1.0.0";
 
-            src = ./.;
-            vendorSha256 = "sha256-Ezt1HDxGQ7DePF90HgHKkH7v365ICFdnfcWJipLhCwQ=";
+          src = ./.;
+          vendorSha256 = "sha256-Ezt1HDxGQ7DePF90HgHKkH7v365ICFdnfcWJipLhCwQ=";
 
-            meta = with pkgs.lib; {
-              description = "Relay prometheus alerts to ntfy";
-              homepage = "https://github.com/pinpox/alertmanager-ntfy";
-              license = licenses.gpl3;
-              maintainers = with maintainers; [ pinpox ];
-            };
+          meta = with pkgs.lib; {
+            description = "Relay prometheus alerts to ntfy";
+            homepage = "https://github.com/pinpox/alertmanager-ntfy";
+            license = licenses.gpl3;
+            maintainers = with maintainers; [ pinpox ];
           };
-
-          mock-hook = pkgs.writeScriptBin "mock-hook" ''
-            #!${pkgs.stdenv.shell}
-            ${pkgs.curl}/bin/curl -X POST -d @mock.json http://$HTTP_ADDRESS:$HTTP_PORT
-          '';
         };
 
-        apps = {
-          mock-hook = flake-utils.lib.mkApp { drv = packages.mock-hook; };
-          alertmanager-ntfy = flake-utils.lib.mkApp { drv = packages.alertmanager-ntfy; };
-        };
+        mock-hook = pkgs.writeScriptBin "mock-hook" ''
+          #!${pkgs.stdenv.shell}
+          ${pkgs.curl}/bin/curl -X POST -d @mock.json http://$HTTP_ADDRESS:$HTTP_PORT
+        '';
+      };
 
-        defaultPackage = packages.alertmanager-ntfy;
-        defaultApp = apps.alertmanager-ntfy;
-      });
+      apps = rec {
+        mock-hook = flake-utils.lib.mkApp { drv = packages.mock-hook; };
+        alertmanager-ntfy = flake-utils.lib.mkApp { drv = packages.alertmanager-ntfy; };
+        defulat = alertmanager-ntfy;
+      };
+
+    });
 }
